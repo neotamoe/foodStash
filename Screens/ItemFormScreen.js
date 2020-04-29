e9a19b6c4d01ba5c6f12b53a1b6ea4f8d1c9cecf
neotamoe <neota.moe@daugherty.com>
2020-04-29 18:54:16 -0500

    connecting input values to button

--- Screens/ItemFormScreen.js
+++ Screens/ItemFormScreen.js
@@ -1,27 +1,39 @@
 import { SafeAreaView, ScrollView, Text, TextInput, View, Button } from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
 import { styles } from '../styles';
 
 const ItemFormScreen = () => {
+  const [ itemName, setItemName ] = useState('')
+  const [ expirationDate, setExpirationDate ] = useState('')
+  const [ quantity, setQuantity ] = useState('0')
+  const saveItem = () => {
+    console.log(`itemName: ${itemName}; expirationDate: ${expirationDate}, quantity: ${quantity}`)
+  }
+
   return (
     <SafeAreaView>
       <ScrollView style={styles.scrollView}>
         <Text style={{ marginBottom: 5 }}>Item</Text>
         <TextInput
           style={styles.basicInput}
+          value={itemName}
+          onChangeText={itemName => setItemName(itemName)}
         />
         <Text style={{ marginBottom: 5 }}>Expiration Date</Text>
         <TextInput
           style={styles.basicInput}
+          value={expirationDate}
+          onChangeText={expirationDate => setExpirationDate(expirationDate)}
         />
         <Text style={{ marginBottom: 5 }}>Quantity</Text>
         <TextInput
           style={styles.basicInput}
-        />
+          value={quantity}
+          onChangeText={quantity => setQuantity(quantity)}        />
         <View style={styles.sectionContainer}>
           <Button 
             title="Add"
-            onPress={() => console.log('add pressed')}  
+            onPress={saveItem}  
           />
         </View>
       </ScrollView>
